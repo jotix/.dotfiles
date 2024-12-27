be401d124f220c2840cc8df80410da1405176b80
jotix <jujodeve@gmail.com>
2024-12-27 18:19:06 -0300

    clone arch-config repo

--- scripts/arch-install/jotix-install.sh
+++ scripts/arch-install/jotix-install.sh
@@ -3,6 +3,9 @@
 ### install ssh key
 /mnt/jtx-ssd/jotix/jotix-install.sh
 
+### install arch-config repo
+git clone git@github.com:jotix/arch-config $HOME/arch-config
+
 ### powerline-go
 go install github.com/justjanne/powerline-go@latest
 
