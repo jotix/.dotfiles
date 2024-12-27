a4ac5313f60628a0d735cc2d95f44a982ec872df
jotix <jujodeve@gmail.com>
2024-12-27 18:02:48 -0300

    Update jotix-install.sh

--- scripts/arch-install/jotix-install.sh
+++ scripts/arch-install/jotix-install.sh
@@ -3,8 +3,5 @@
 ### powerline-go
 go install github.com/justjanne/powerline-go@latest
 
-### google-chrome
-flatpak install flathub com.google.Chrome
-
 ### gnome extension manager
 flatpak install flathub com.mattjakeman.ExtensionManager
