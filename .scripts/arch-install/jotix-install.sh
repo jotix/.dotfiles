025c8000ae0e732ead50fb2ab8b21c5557679858
jotix <jujodeve@gmail.com>
2024-12-27 19:45:41 -0300

    refactoring for stow

--- .scripts/arch-install/jotix-install.sh
+++ .scripts/arch-install/jotix-install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-### install ssh key
-/mnt/jtx-ssd/jotix/jotix-install.sh
-
-### install arch-config repo
-git clone git@github.com:jotix/arch-config $HOME/arch-config
-
 ### powerline-go
 go install github.com/justjanne/powerline-go@latest
 
---
+++ unstow.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+find .  \( -type f -o -type l \) -print0 | sed -z "s|^.|$HOME|" | xargs -0 -I{} rm {}
