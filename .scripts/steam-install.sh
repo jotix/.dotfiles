c5375c4c2a97283d9245a6a9eb2c3c45e7b22e9e
jotix <jujodeve@gmail.com>
2024-12-28 10:47:16 -0300

    minion

--- .scripts/steam-install.sh
+++ .scripts/steam-install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-### steam 
-
+### steam
 flatpak install com.valvesoftware.Steam
+
+### minion
+flatpak install gg.minion.Minion
+
+### filesystems override
 flatpak override --filesystem=/mnt/jtx-ssd com.valvesoftware.Steam --user
-flatpak override --filesystem=/mnt/jtx-nvme com.valvesoftware.Steam --user
+flatpak override --filesystem=/mnt/jtx-ssd gg.minion.Minion --user
+if [[ -b "/dev/disk/by-label/jtx-nvme" ]]; then
+    flatpak override --filesystem=/mnt/jtx-nvme com.valvesoftware.Steam --user
+    flatpak override --filesystem=/mnt/jtx-nvme gg.minion.Minion --user
+fi
