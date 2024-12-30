31679255ccc71751743801bfc21c44438998d475
jotix <jujodeve@gmail.com>
2024-12-29 21:40:50 -0300

    steam install

--- .scripts/arch-install/jotix-install.sh
+++ .scripts/arch-install/jotix-install.sh
@@ -19,3 +19,17 @@ go install github.com/justjanne/powerline-go@latest
 
 ### gnome extension manager
 flatpak install flathub com.mattjakeman.ExtensionManager
+
+### steam
+flatpak install com.valvesoftware.Steam
+
+### minion
+flatpak install gg.minion.Minion
+
+### filesystems override
+flatpak override --filesystem=/mnt/jtx-ssd com.valvesoftware.Steam --user
+flatpak override --filesystem=/mnt/jtx-ssd gg.minion.Minion --user
+if [[ -b "/dev/disk/by-label/jtx-nvme" ]]; then
+    flatpak override --filesystem=/mnt/jtx-nvme com.valvesoftware.Steam --user
+    flatpak override --filesystem=/mnt/jtx-nvme gg.minion.Minion --user
+fi
