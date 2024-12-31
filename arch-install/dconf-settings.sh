c586a7ec330e0b4d284430be14db823a181c3089
jotix <jujodeve@gmail.com>
2024-12-30 21:12:56 -0300

    dconf settings

--- arch-install/dconf-settings.sh
+++ arch-install/dconf-settings.sh
@@ -8,7 +8,20 @@ dconf write /org/gnome/desktop/screensaver/lock-enabled false
 dconf write /org/gnome/desktop/notifications/show-banners false
 dconf write /org/gnome/desktop/wm/preferences/button-layout "'appmenu:minimize,maximize,close'"
 dconf write /org/gnome/Console/last-window-size '(1200, 900)'
-dconf write /org/gnome/shell/favorite-apps "['com.google.Chrome.desktop', \
-'org.gnome.Nautilus.desktop', 'dev.zed.Zed.desktop', 'org.gnome.Extensions.desktop', \
-'org.gnome.Console.desktop', 'com.valvesoftware.Steam.desktop', 'org.gnome.Settings.desktop', \
-'com.mattjakeman.ExtensionManager.desktop']"
+dconf write /org/gnome/shell/favorite-apps "[\
+    'com.google.Chrome.desktop', \
+    'org.gnome.Nautilus.desktop', \
+    'dev.zed.Zed.desktop', \
+    'org.gnome.Console.desktop', \
+    'com.valvesoftware.Steam.desktop', \
+    'org.gnome.Settings.desktop', \
+    'com.mattjakeman.ExtensionManager.desktop']"
+
+dconf write /org/gnome/desktop/interface/cursor-theme "'Adwaita'"
+dconf write /org/gnome/desktop/interface/icon-theme "'Adwaita'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita'"
+
+dconf write /org/gnome/shell/enabled-extensions "[\
+    'dash-to-dock@micxgx.gmail.com', \
+    'apps-menu@gnome-shell-extensions.gcampax.github.com', \
+    'tiling-assistant@leleat-on-github']"
