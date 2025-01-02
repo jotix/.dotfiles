fd433484b8ebdf6fa6602895d0827eb0e2c09887
jotix <jujodeve@gmail.com>
2025-01-02 20:17:58 -0300

    Update jotix-install.sh

--- arch-install/jotix-install.sh
+++ arch-install/jotix-install.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-DESKTOP=""
-
-while [[ $DESKTOP != "plasma" && $DESKTOP != "gnome" ]]
-do
-  read -p "Wich Desktop Enviroment install? (plasma/gnome): " DESKTOP
-done
-
-if [[ DESKTOP == "plasma" ]]; then
-  sudo pacman -S --noconfirm --needed plasma kde-applications kitty system-config-printer tesseract-data-eng
-  sudo systemctl enable sddm
-else
-  sudo pacman -S --noconfirm --needed gnome gnome-extra gnome-browser-connector ghostty
-fi
-
 ### powerline-go
 go install github.com/justjanne/powerline-go@latest
 
